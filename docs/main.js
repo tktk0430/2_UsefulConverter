a128655a35ba5737a194fdcc655af86624a5f69c
Taku <h.taku.0430.tech@gmail.com>
2019-02-16 00:51:47 +0900

    単位多数追加、スペルミス修正、リセットボタン追加、表示桁数変更、etc

--- docs/main.js
+++ docs/main.js
@@ -1,8 +1,3 @@
-var unitArray ={
-    length:{mm:0.001,m:1,km:1000,yard:0.9144,yd:0.9144,ft:0.3048,in:0.0254},
-    weight:{mg:0.001,g:1,kg:1000,t:1000000,lb:453.6}
-};
-
 var leftValue=document.getElementById("left-value");
 var leftUnit=document.getElementById("left-unit");
 var rightValue=document.getElementById("right-value");
@@ -26,11 +21,18 @@ document.getElementById("calc").addEventListener("click",function(){
     noteTarget=document.getElementById("note")
     if (myUnit){
         if (leftValue.value!=""){
-            rightValue.value=leftValue.value*unitArray[myUnit][leftUnit.value]/unitArray[myUnit][rightUnit.value];
+            rightValue.value=(leftValue.value*unitArray[myUnit][leftUnit.value]/unitArray[myUnit][rightUnit.value]).toFixed(4);
         }else{
-            leftValue.value=rightValue.value*unitArray[myUnit][rightUnit.value]/unitArray[myUnit][leftUnit.value];
+            leftValue.value=(rightValue.value*unitArray[myUnit][rightUnit.value]/unitArray[myUnit][leftUnit.value]).toFixed(4);
         };
         noteTarget.innerHTML=myUnit;
     }else{
-        noteTarget.innerHTML="Units are different or blank."
+        noteTarget.innerHTML="Units are different,undefined,or blank."
     }});
+
+document.getElementById("reset").addEventListener("click",function(){
+    leftUnit.value="";
+    leftValue.value="";
+    rightUnit.value="";
+    rightValue.value="";
+});
